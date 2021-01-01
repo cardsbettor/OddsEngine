c9b24ac8b5f485bb6d64f3749d2f66e3985cbf46
cardsbettor <thecardsbettor@outlook.com>
2021-01-01 23:13:03 +0200

    Update GetExpects.R

--- GetExpects.R
+++ GetExpects.R
@@ -3,18 +3,16 @@ GetExpects <- function(hcapLine,homeOdds,awayOdds,totalsLine,overOdds,underOdds)
   Exp <- data.frame(matrix(nrow=length(hcapLine),ncol=2))
   names(Exp) <- c("Home","Away")
 
-  if(is.na(homeOdds) || is.na(overOdds)){
-    Exp[[1]] <- NA
-    Exp[[2]] <- NA
-  }  else{
-    homeFair <- Demarg(homeOdds,awayOdds)[[1]]
-    underFair <- Demarg(overOdds,underOdds)[[2]]
-
-    total <- ExpTotal(totalsLine,underFair)
-
-    Exp[1] <- ExpHome(hcapLine,total,homeFair)
-    Exp[2] <- total-Exp[1]
-  }
+  Exp$Home <- ifelse(is.na(homeOdds) | is.na(overOdds),
+                     NA,
+                     ExpHome(hcapLine,ExpTotal(totalsLine,Demarg(overOdds,underOdds)[[2]]),
+                             Demarg(homeOdds,awayOdds)[[1]]
+                             )
+                     )
+  Exp$Away <- ifelse(is.na(homeOdds) | is.na(overOdds),
+                     NA,
+                     ExpTotal(totalsLine,Demarg(overOdds,underOdds)[[2]])-Exp$Home
+                     )
 
   Exp
 }
